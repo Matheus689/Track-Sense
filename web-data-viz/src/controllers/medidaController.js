6fbc64788ee8b4d3fb3093bda5375b016bd3919b
Nathan-Martins3010 <nathan.martins@sptech.school>
2026-05-18 22:15:24 -0300

    Integração da dashboard com o banco de dados

--- web-data-viz/src/controllers/medidaController.js
+++ web-data-viz/src/controllers/medidaController.js
@@ -24,11 +24,35 @@ function buscarUltimasMedidas(req, res) {
 
 function buscarMedidasEmTempoReal(req, res) {
 
-    var idAquario = req.params.idAquario;
+    var idSensor = req.params.idSensor;
 
-    console.log(`Recuperando medidas em tempo real`);
+    console.log("Recuperando medidas em tempo real");
 
-    medidaModel.buscarMedidasEmTempoReal(idAquario).then(function (resultado) {
+    medidaModel.buscarMedidasEmTempoReal(idSensor)
+        .then(function (resultado) {
+
+            if (resultado.length > 0) {
+                res.status(200).json(resultado);
+            } else {
+                res.status(204).send("Nenhum resultado encontrado!");
+            }
+
+        }).catch(function (erro) {
+
+            console.log(erro);
+            console.log("Erro ao buscar medidas:", erro.sqlMessage);
+
+            res.status(500).json(erro.sqlMessage);
+
+        });
+}
+
+function buscarTodasMaquinas (req, res){
+    var idEmpresa = req.params.idEmpresa;
+
+    console.log(`Buscando máquinas da empresa:`, idEmpresa);
+
+    medidaModel.buscarTodasMaquinas(idEmpresa).then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
@@ -41,7 +65,45 @@ function buscarMedidasEmTempoReal(req, res) {
     });
 }
 
+function buscarProducaoGeral(req, res) {
+
+    var idEmpresa = req.params.idEmpresa;
+
+    medidaModel.buscarProducaoGeral(idEmpresa)
+        .then(function(resultado){
+
+            res.status(200).json(resultado);
+
+        }).catch(function(erro){
+
+            console.log(erro);
+            res.status(500).json(erro.sqlMessage);
+
+        });
+}
+
+function buscarProducaoDiariaMaquina(req, res) {
+
+    var idEmpresa = req.params.idEmpresa;
+
+    medidaModel.buscarProducaoDiariaMaquina(idEmpresa)
+        .then(function(resultado){
+
+            res.status(200).json(resultado);
+
+        }).catch(function(erro){
+
+            console.log(erro);
+            res.status(500).json(erro.sqlMessage);
+
+        });
+}
+
+
 module.exports = {
     buscarUltimasMedidas,
-    buscarMedidasEmTempoReal
+    buscarMedidasEmTempoReal,
+    buscarTodasMaquinas,
+    buscarProducaoGeral,
+    buscarProducaoDiariaMaquina
 }
--- web-data-viz/src/routes/medidas.js
+++ web-data-viz/src/routes/medidas.js
@@ -15,4 +15,17 @@ router.get("/producao-real/:idSensor", function (req, res) {
     medidaController.buscarMedidasEmTempoReal(req, res);
 })
 
+// dashboard
+router.get("/maquinas/:idEmpresa", function (req, res) {
+    medidaController.buscarTodasMaquinas(req, res);
+});
+
+router.get("/producao-geral/:idEmpresa", function(req, res){
+    medidaController.buscarProducaoGeral(req, res);
+});
+
+router.get("/producao-diaria/:idEmpresa", function(req, res){
+    medidaController.buscarProducaoDiariaMaquina(req, res);
+});
+
 module.exports = router;
